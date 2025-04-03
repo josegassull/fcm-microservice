2474a1aaa2dd050b34883d8df24a7edc8a88cb55
stf <sstefanofm@gmail.com>
2025-04-03 14:37:26 -0300

    starter springboot project. Using java 24

---
+++ token-api/src/test/java/com/fcm_ms/token_api/TokenApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fcm_ms.token_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TokenApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

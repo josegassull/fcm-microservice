ea6518e0e1a963ddd93f5135102f5ebaad382e7a
stf <sstefanofm@gmail.com>
2025-06-08 19:54:41 -0300

    added HttpStatus field for notificationResponse

--- token-api/src/main/java/com/fcm_ms/token_api/dto/NotificationResponseDTO.java
+++ token-api/src/main/java/com/fcm_ms/token_api/dto/NotificationResponseDTO.java
@@ -4,6 +4,9 @@ import java.util.Map;
 import java.time.LocalDateTime;
 import lombok.Builder;
 import lombok.Data;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+import org.springframework.http.HttpStatus;
 
 @Data
 @Builder
@@ -12,6 +15,19 @@ public class NotificationResponseDTO {
   private LocalDateTime timestamp;
   private String message;
   private Map<String, Integer> notifiedDevicesCounts;
+  private HttpStatusDetail httpStatus;
+
+  public HttpStatus _getHttpStatus() {
+    return HttpStatus.valueOf(this.httpStatus.getName());
+  }
+
+  @Data
+  @AllArgsConstructor
+  @NoArgsConstructor
+  public static class HttpStatusDetail {
+    private String name;
+    private Integer code;
+  }
 
   public static NotificationResponseDTO of(
     int totalMessages,
@@ -24,10 +40,17 @@ public class NotificationResponseDTO {
       "failure", failureDevices
     );
 
+    HttpStatus httpStatus = failureDevices == 0 ?
+      HttpStatus.OK :
+      HttpStatus.PARTIAL_CONTENT;
+
     return NotificationResponseDTO.builder()
       .timestamp(LocalDateTime.now())
       .message("Fired notifications for " + totalMessages + " instances of user " + userExternalId)
       .notifiedDevicesCounts(counts)
+      .httpStatus(new HttpStatusDetail(
+        httpStatus.name(), httpStatus.value()
+      ))
       .build();
   }
 }

87575f5591ae01664f9f6a4d860e83977f233524
stf <sstefanofm@gmail.com>
2025-06-08 16:53:31 -0300

    update config for FirebaseApp

--- token-api/src/main/java/com/fcm_ms/token_api/config/FirebaseConfig.java
+++ token-api/src/main/java/com/fcm_ms/token_api/config/FirebaseConfig.java
@@ -5,6 +5,7 @@ import java.io.IOException;
 import com.google.auth.oauth2.GoogleCredentials;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
+import com.google.firebase.messaging.FirebaseMessaging;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.ClassPathResource;
@@ -14,15 +15,20 @@ public class FirebaseConfig {
 
   @Bean
   public FirebaseApp firebaseApp() throws IOException {
+    if (!FirebaseApp.getApps().isEmpty())
+      return FirebaseApp.getInstance();
+
     ClassPathResource resource = new ClassPathResource("firebase-service-account.json");
 
     FirebaseOptions options = FirebaseOptions.builder()
       .setCredentials(GoogleCredentials.fromStream(resource.getInputStream()))
       .build();
 
-    if (FirebaseApp.getApps().isEmpty())
-      return FirebaseApp.initializeApp(options);
-    else
-      return FirebaseApp.getInstance();
+    return FirebaseApp.initializeApp(options);
+  }
+
+  @Bean
+  public FirebaseMessaging firebaseMessaging(FirebaseApp firebaseApp) {
+    return FirebaseMessaging.getInstance(firebaseApp);
   }
 }

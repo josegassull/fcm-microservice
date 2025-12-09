3f76fb2e93ee5fc965ae3914d24c3d42d9f5635f
jose <josegassull@gmail.com>
2025-12-09 12:26:17 -0300

    Deleted the cross-origins that was used for testing in the code in TokenController.java

--- token-api/src/main/java/com/fcm_ms/token_api/controller/TokenController.java
+++ token-api/src/main/java/com/fcm_ms/token_api/controller/TokenController.java
@@ -16,9 +16,6 @@ public class TokenController {
 
   private final TokenService tokenService;
 
-  //TODO: SACAR EL CROSS
-  @CrossOrigin(origins = "*")
-  @PostMapping("register")
   public ResponseEntity<TokenResponseDTO> registerNewToken(@Valid @RequestBody TokenRequestDTO tokenRequest) {
     Boolean isCreated = this.tokenService.registerToken(tokenRequest);
     TokenResponseDTO tokenResponse = this.tokenService.getTokenResponse(tokenRequest, isCreated);

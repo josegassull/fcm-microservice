1bbbd1bf1e4ebf861539f3b083c583a3d4b634c4
Stefano <stf@manjaro-cumbre.lan>
2025-12-11 10:32:08 -0300

    created DataMessageRequestDTO and validating data

--- token-api/src/main/java/com/fcm_ms/token_api/controller/UserDataMessageController.java
+++ token-api/src/main/java/com/fcm_ms/token_api/controller/UserDataMessageController.java
@@ -3,16 +3,19 @@ package com.fcm_ms.token_api.controller;
 import java.util.Optional;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import lombok.RequiredArgsConstructor;
 import com.google.firebase.messaging.FirebaseMessaging;
+import jakarta.validation.Valid;
 import jakarta.servlet.http.HttpServletRequest;
 
 import com.fcm_ms.token_api.util.StringToIntUtil;
 import com.fcm_ms.token_api.dto.ErrorResponseDTO;
+import com.fcm_ms.token_api.dto.DataMessageRequestDTO;
 
 @RestController
 @RequestMapping("api/data-message/user")
@@ -24,6 +27,7 @@ public class UserDataMessageController {
   @PostMapping("{user_external_id}")
   public String dataMessageUser(
     @PathVariable("user_external_id") String userExternalId,
+    @Valid @RequestBody DataMessageRequestDTO dataMessageRequestDTO,
     HttpServletRequest request) {
 
     Optional<ErrorResponseDTO> existingError = StringToIntUtil.intOrErrorDTO(
